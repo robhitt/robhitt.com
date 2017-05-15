ca47047894d906bb19661b3ae8e01d4536227e5f
Rob Hitt <robhitt@gmail.com>
2017-05-15 12:20:40 -0400

    index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
+$(document).ready(function () {
+  var botURL = 'https://api.motion.ai/webchat/45969?color=62a8ea&sendBtn=SEND&inputBox=Type%20something...&token=546e701daf2fbeea679c5d078abbb1e6';
 
-  document.getElementById("mainDiv").addEventListener("mouseover", displayDate);
 
-  function displayDate() {
-      var currDate = Date();
-      document.getElementById("demo").innerText = currDate;
-      setTimeout(displayName, 2000);
-  }
+  // document.getElementById("mainDiv").addEventListener("mouseover", displayDate);
+  //
+  // function displayDate() {
+  //     var currDate = Date();
+  //
+  //     // document.getElementById("demo").innerText = currDate;  javascript
+  //     $("#demo").text(currDate);
+  //
+  //     setTimeout(displayName, 2000);
+  // }
 
-  function displayName() {
-      var name = "<h1>Rob Hitt</h1>"
-      document.getElementById("demo").innerHTML = name;
-  }
+  // function displayName() {
+  //     var name = "<h1>Rob Hitt</h1>";
+  //     document.getElementById("demo").innerHTML = name;
+  // }
+
+  $('.container').hide();
+  $('.container').fadeIn(500);
+
+});
